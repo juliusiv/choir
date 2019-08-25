e6a8dc5944bc88a7aa8b8c72dcdc08847745f71d
Julius Alexander IV <alexaj5rpi@gmail.com>
2019-08-25 11:15:39 +0800

    Start from scratch with a new phoenix version.

--- test/controllers/page_controller_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Choir.PageControllerTest do
-  use Choir.ConnCase
-
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "choir"
-  end
-end
