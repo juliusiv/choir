75d16f10939c74bf1c845f9be289e90d847bd544
Julius Alexander IV <alexaj5rpi@gmail.com>
2019-08-29 21:00:03 +0800

    Add login and signup backends.

--- lib/choir/users/user.ex
+++ lib/choir/users/user.ex
@@ -1,12 +1,13 @@
 defmodule Choir.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecto.UUID
 
   schema "user" do
-    field :email, :string
-    field :name, :string
-    field :password, :string
-    field :uid, :string
+    field(:email, :string)
+    field(:name, :string)
+    field(:password, :string)
+    field(:uid, :string)
 
     timestamps()
   end
@@ -15,6 +16,7 @@ defmodule Choir.Users.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :name, :password, :uid])
+    |> put_change(:uid, UUID.generate())
     |> validate_required([:email, :name, :password, :uid])
   end
 end
