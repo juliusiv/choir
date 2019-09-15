1577b780f8f7a7713458cb44e04bc67fb2f2d54f
Julius Alexander IV <alexaj5rpi@gmail.com>
2019-09-15 12:07:05 +0800

    Reorganize some frontend file structure. (#12)

--- assets/src/pages/Rehearse/index.js
+++ assets/src/pages/Rehearse/index.js
@@ -5,7 +5,7 @@ import PageContainer, { Pages } from "<choir>/containers/PageContainer";
 class Rehearse extends React.Component {
     render() {
         return (
-            <PageContainer withNavigation={true} page={Pages.ABOUT}>
+            <PageContainer withNavigation={true} page={Pages.REHEARSE}>
                 rehearse here
             </PageContainer>
         );
