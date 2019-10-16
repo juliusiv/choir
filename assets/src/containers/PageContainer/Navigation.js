18a3919707c34d6c8cb749ec4d376ffc888e7af8
Julius Alexander IV <alexaj5rpi@gmail.com>
2019-10-15 23:52:36 -0700

    Add the icon.

--- assets/src/containers/PageContainer/Navigation.js
+++ assets/src/containers/PageContainer/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 
+import Icon from "<choir>/components/Icon";
 import Link from "<choir>/components/Link";
 import css from "<style>";
 import Pages from "./Pages";
@@ -39,8 +40,10 @@ const PAGE_LINKS = [
 const Navigation = ({ active, ...props }) => {
     return (
         <nav className={css`bgBlack cWhite`} {...props}>
-            <Link to="/" className={css`pl2 pr2 pb3 pt3 bgRed cWhite bold block fontMedium cursorPointer underlineNone`}>
-                Choir
+            <Link to="/" className={css`pl2 pr2 pb3 pt3 bgRed cWhite bold block fontMedium cursorPointer underlineNone flex justifyCenter`}>
+                <div style={{ height: "2rem" }}>
+                    <Icon icon="logo" color="var(--chr-white)" />
+                </div>
             </Link>
             {
                 PAGE_LINKS.map(({ to, page, title }) => (
